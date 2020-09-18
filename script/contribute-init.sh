e068a5f9ae7085f38a847a53b3e2dec9376dbb14
Francesco Bianco <info.francescobianco@gmail.com>
2020-09-18 13:17:27 +0200

    add file

---
+++ script/contribute-init.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+[[ -d "CLONE" ]] || git clone https://github.com/javanile/vtiger-styleci.git CLONE
+
+cd CLONE
+
+git symbolic-ref HEAD refs/heads/contribute
+git rm -rf . && true
+rm -r . && true
+git pull https://code.vtiger.com/vtiger/vtigercrm.git master -X theirs --no-edit --allow-unrelated-histories
+git push --set-upstream origin contribute
